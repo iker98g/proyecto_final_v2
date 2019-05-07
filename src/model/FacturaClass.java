bf6eb2a0ef85f20060a9dbbda5354e0efa3da9a2
iker gomez <iker98g@gmail.com>
2019-05-07 11:10:24 +0200

    commit de FacturaClass y FacturaModel

--- src/model/FacturaClass.java
+++ src/model/FacturaClass.java
@@ -1,5 +1,34 @@
 package model;
 
-public class FacturaClass {
+import java.sql.Date;
+
+public class FacturaClass extends Connect{
+	
+	protected int idFactura;
+	protected Date fecha;
+	
+	//Constructores
+	public FacturaClass(int idFactura, Date fecha) {
+		super();
+		this.idFactura = idFactura;
+		this.fecha = fecha;
+	}
+	public FacturaClass() {
+		super();
+	}
+	
+	//Getters and Setters
+	public int getIdFactura() {
+		return idFactura;
+	}
+	public void setIdFactura(int idFactura) {
+		this.idFactura = idFactura;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
 
 }
--- src/model/FacturaModel.java
+++ src/model/FacturaModel.java
@@ -1,5 +1,56 @@
 package model;
 
-public class FacturaModel {
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
+public class FacturaModel extends FacturaClass{
+	
+	private ArrayList<FacturaClass> list = new ArrayList<FacturaClass>();
+
+	//Constructores
+	public FacturaModel(int idFactura, Date fecha, ArrayList<FacturaClass> list) {
+		super(idFactura, fecha);
+		this.list = list;
+	}
+
+	public FacturaModel() {
+		super();
+	}
+
+	//Getters and Setters
+	public ArrayList<FacturaClass> getList() {
+		return list;
+	}
+
+	public void setList(ArrayList<FacturaClass> list) {
+		this.list = list;
+	}
+	
+	public void loadData()
+	{
+		this.CreateConnection();
+		
+		Statement st;
+		try {
+			st = this.con.createStatement();
+			ResultSet rs = st.executeQuery("SELECT * FROM Factura ");
+
+			while (rs.next()) // reads the table line by line
+			{
+				FacturaModel newF = new FacturaModel();
+				newF.idFactura=Integer.parseInt(rs.getString(1));
+				newF.fecha=rs.getDate(2);
+				this.list.add(newF);
+			}
+	
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		this.disconnect();
+	}
+	
 }
