b3c12b9979331685869f8d3eaadd1f5ad7fafb7a
Bogdan <bogdan@gmail.com>
2019-05-07 14:58:45 +0200

    servlet a medias

--- src/controller/CProductos.java
+++ src/controller/CProductos.java
@@ -1,11 +1,16 @@
 package controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+import org.json.JSONStringer;
 
 import model.ProductosModel;
 
@@ -28,8 +33,30 @@ public class CProductos extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ProductosModel productosModelo=new ProductosModel();
-		ArrayList<Producto> productos = ProductosModel.lo
+		ProductosModel myProductos=new ProductosModel();
+		myProductos.loadData();
+		
+		String jsonString = JSONStringer.valueToString(myProductos);
+			
+ 		for (int i=0;i<myProductos.getList().size();i++){
+ 			
+ 			JSONObject jsonObject=new JSONObject();
+ 			jsonObject.put("nombre", myProductos.getList().get(i).getNombre());
+ 			jsonObject.put("idCategoria", myProductos.getList().get(i).getIdCategoria());
+ 			jsonObject.put("idprecio", myProductos.getList().get(i).getIdCategoria());
+ 			jsonObject.put("Descripcion", myProductos.getList().get(i).getDescripcion());
+ 			jsonObject.put("imagen", myProductos.getList().get(i).getImagen());
+ 			jsonString.add(jsonObject);	
+ 		}		
+
+		PrintWriter out = response.getWriter();
+	
+		response.setHeader("Access-Control-Allow-Origin","*"); //jsonp deia denean ez da behar
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+			
+		out.print(jsonString);
+		out.flush();
 		
 	}
 
