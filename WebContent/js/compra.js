867a19b2716406db3c1a94baab54f5cec12749b6
iker gomez <iker98g@gmail.com>
2019-05-14 10:58:45 +0200

    carrito funciona

---
+++ WebContent/js/compra.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+   
+	var vCarrito=JSON.parse(localStorage.getItem('carrito'));
+	var htmlCode="";
+	
+	for (let i = 0; i < vCarrito.length; i++) {
+	
+	htmlCode += '<tr>';
+    htmlCode += 	'<td><img src="'+ vCarrito[i].imagen +'"/></td>';
+    htmlCode += 	'<td>'+ vCarrito[i].nombre +'</td>';
+    htmlCode += 	'<td>'+ vCarrito[i].precio +'</td>';
+    htmlCode += '</tr>';
+	}
+	$('#compra > tbody').html(htmlCode);
+});
